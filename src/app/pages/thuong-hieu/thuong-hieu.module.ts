d32b5b248a4a32a0a99f1b603114c517c6a85604
xileoxmax <nguyenanhminh150598@gmail.com>
2020-08-18 14:31:40 +0700

    search complete

--- src/app/pages/thuong-hieu/thuong-hieu.module.ts
+++ src/app/pages/thuong-hieu/thuong-hieu.module.ts
@@ -4,14 +4,18 @@ import { ThuongHieuComponent } from './thuong-hieu.component';
 import { RouterModule } from '@angular/router';
 import { ThuongHieuRoutes } from './thuong-hieu.routing';
 import { NgxPaginationModule } from 'ngx-pagination';
+import { FilterPipe } from 'src/pipe/filter.pipe';
+import { FormsModule } from '@angular/forms';
+import { HighlightDirective } from 'src/app/directive/highlight.directive';
+
 @NgModule({
   imports: [
-    CommonModule,RouterModule.forChild(ThuongHieuRoutes),
-    NgxPaginationModule
+    CommonModule,
+    RouterModule.forChild(ThuongHieuRoutes),
+    NgxPaginationModule,
+    FormsModule,
   ],
-  declarations: [
-    ThuongHieuComponent,
-  ],
+  declarations: [ThuongHieuComponent, FilterPipe, HighlightDirective],
   exports: [],
 })
-export class ThuongHieuModule { }
+export class ThuongHieuModule {}
